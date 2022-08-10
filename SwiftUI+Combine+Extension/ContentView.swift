2cd90fbb0956db80b8719f88bce6aaa1b4300b87
Anirudha Mahale <anirudhamahale@gmail.com>
2022-08-10 12:22:35 +0530

    Initial Commit

---
+++ SwiftUI+Combine+Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI+Combine+Extension
+//
+//  Created by Anirudha Mahale on 10/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI+Combine+Extension/SwiftUI_Combine_ExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Combine_ExtensionApp.swift
+//  SwiftUI+Combine+Extension
+//
+//  Created by Anirudha Mahale on 10/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Combine_ExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
